ada55d8c7af2dab47accf10861b96fb573a0bbe4
Ayush Mandowara <ayushxx7@gmail.com>
2017-04-09 16:22:10 +0530

    basic liker
    files uploaded, including demo video

---
+++ autoLikeFB.py
@@ -0,0 +1,40 @@
+import time
+import pyautogui
+'''
+pyautogui.hotkey('alt','tab')
+time.sleep(2)
+x1 = 224
+y1 = 499
+m =258
+cnt = 0
+for l in range(0,20):
+    for z in range(0,4):
+        pyautogui.moveTo(x1,y1)
+        time.sleep(0.5)
+        x1 += 210
+    x1 = 224
+    pyautogui.moveTo(x1,y1)
+    time.sleep(0.8)
+    pyautogui.scroll(-m)
+    cnt += 1
+    if cnt == 2:
+        m -= 4
+        cnt = 0
+
+
+#pyautogui.scroll(-760)
+
+# 760 scroll difference b/w 1st and 4th image line distance
+'''
+
+pyautogui.hotkey('alt', 'tab')
+for z in range(0,100):
+    time.sleep(0.2)
+    if (pyautogui.locateOnScreen('likeButtonOnFB.png')) != None:
+        pyautogui.press('l')
+    pyautogui.press('right')
+    time.sleep(0.2)
+
+'''
+<a aria-pressed="false" class="UFILikeLink _4x9- _4x9_ _48-k" data-testid="fb-ufi-likelink" href="#" role="button" tabindex="-1"><!-- react-text: 8 -->Like<!-- /react-text --></a>
+'''
---
+++ readme.md
@@ -0,0 +1,9 @@
+A very simple and hacky Image AutoLiker Bot for Facebook.
+Uses PyAutoGui extensively.
+User needs to be logged into their account, and first image should be opened up. This state should be immediately switched to script runner[such as  CMD/PyCharm] (using alt-tab).
+Number of images is specified inside the script itself.
+The image [likeButtonOnFB] is unique for each monitor, take your own screen shot and replace with file present.
+
+
+Ideas for betterment will be highly appreciated.
+
